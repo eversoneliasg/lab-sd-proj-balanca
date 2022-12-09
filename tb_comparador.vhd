b80653cc1bddc64bc254d720046c0c4dc9d94209
Alex C. Souza <alexdcesouza@gmail.com>
2022-12-08 23:04:24 -0300

    Adicionado tb para comparador

--- tb_comparador.vhd
+++ tb_comparador.vhd
@@ -1,29 +1,30 @@
 library IEEE;
 
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.std_logic_1164.ALL;
+use IEEE.std_logic_ARITH.ALL;
+use IEEE.std_logic_UNSIGNED.ALL;
 
 ENTITY tb_comparador IS
-generic
-	(
-		DATA_WIDTH : natural := 16
-	);
+generic(
+		W_16 : natural := 16
+);
 END tb_comparador;
 
 ARCHITECTURE teste OF tb_comparador IS
-    COMPONENT comparador IS
-        port(   A:IN STD_LOGIC_VECTOR((DATA_WIDTH-1) DOWNTO 0);
-                B:IN STD_LOGIC_VECTOR((DATA_WIDTH-1) DOWNTO 0);
-                output:OUT STD_LOGIC
-        );
-    END COMPONENT;
+   COMPONENT comparador IS
+   port(   
+		A : in std_logic_vector((W_16-1) downto 0);
+      B : in std_logic_vector((W_16-1) downto 0);
+      output : out std_logic
+   );
+   END COMPONENT;
 
-    SIGNAL a : STD_LOGIC_VECTOR((DATA_WIDTH-1) DOWNTO 0);
-    SIGNAL b : STD_LOGIC_VECTOR((DATA_WIDTH-1) DOWNTO 0);
-    SIGNAL c : STD_LOGIC;
+   SIGNAL a : std_logic_vector((W_16-1) downto 0);
+   SIGNAL b : std_logic_vector((W_16-1) downto 0);
+   SIGNAL c : std_logic;
+
 BEGIN
-    uut : comparador PORT MAP(a, b , c);
+    uut : comparador port map(a, b, c);
     a <= "0000000000000001", "0000000000000111" after 20 ns, "0000000000000110" after 40 ns, "0000000000001100" after 60 ns, "0000000000010100" after 70 ns;
 	 b <= "0000000000000000", "0000000000000011" after 20 ns, "0000000000000010" after 40 ns, "0000000000000100" after 60 ns, "0000000000000100" after 70 ns;
 END teste;
